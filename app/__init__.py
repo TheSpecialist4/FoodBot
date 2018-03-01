3357840c173f9eea1d6f1a2731f4ab90c1ca97fb
kaamraan kamaal <kaamraank@outlook.com>
2018-03-01 17:09:01 +0530

    restructure routes to a new file

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,14 @@ from flask_migrate import Migrate
 
 db = SQLAlchemy()
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///foodbot.db'
-    db.init_app(app)
-    
-    migrate = Migrate(app, db)
+#def create_app():
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///foodbot.db'
+db.init_app(app)
 
-    from app import models
+migrate = Migrate(app, db)
 
-    return app
+from app import models
+from app import routes
+
+#    return app
