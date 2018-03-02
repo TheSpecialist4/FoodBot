824c3b3a017abe147c806fe98d6bf50fe38dac32
kaamraan kamaal <kaamraank@outlook.com>
2018-03-03 02:10:28 +0530

    add search form with query functionality

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,15 @@
 from app import app
+from app import request
 
 @app.route('/')
 @app.route('/home/')
 def home():
-    return 'Home page'
+    return '<form action = "http://localhost:5000/search/" method = "POST"><input type = "text" name = "query" placeholder = "Search.."/><input type = "submit" value = "submit"/></form>'
+
+@app.route('/search/', methods = ['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        print(request.form['query'])
+        return request.form['query']
+    else:
+        return redirect(url_for('home'))
